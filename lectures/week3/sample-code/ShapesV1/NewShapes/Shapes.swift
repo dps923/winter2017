0dc9b16505029a1948e53b02f903df2bab36b43b
Garvan Keeley <garvankeeley@gmail.com>
2017-01-19 18:18:30 -0500

    add sample code for week3 lecture

---
+++ lectures/week3/sample-code/ShapesV1/NewShapes/Shapes.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  NewShapes
+//
+//  Created by Peter McIntyre on 2015-04-05.
+//  Copyright (c) 2015 Seneca College. All rights reserved.
+//
+
+import UIKit
+
+class Shapes: UIViewController {
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - User interface actions
+
+    // Create and configure instances of our custom view object
+
+    @IBAction func toggleSquare(_ sender: UISwitch) {
+
+        if sender.isOn {
+            
+            // Create a shape, first two arguments are relative to the upper-left corner
+            // The last two arguments are the x-width and the y-height
+            let newShape = Shape(frame: CGRect(x: 20, y: 110, width: 100, height: 100))
+            
+            // Configure the properties
+            newShape.shapeTypeLabel = "Square"
+            newShape.shapeColor = UIColor.orange.cgColor
+            newShape.tag = 1
+            
+            // Add the new shape to the view
+            self.view.addSubview(newShape)
+
+        } else {
+            
+            // Locate the shape to be removed
+            let shapeToBeRemoved = self.view.viewWithTag(1)
+            // Remove the shape
+            shapeToBeRemoved?.removeFromSuperview()
+        }
+    }
+    
+    @IBAction func toggleRectangle(_ sender: UISwitch) {
+
+        if sender.isOn {
+            let newShape = Shape(frame: CGRect(x: 60, y: 150, width: 100, height: 225))
+            newShape.shapeTypeLabel = "Rectangle"
+            newShape.shapeColor = UIColor.red.cgColor
+            newShape.tag = 2
+            self.view.addSubview(newShape)
+        } else {
+            let shapeToBeRemoved = self.view.viewWithTag(2)
+            shapeToBeRemoved?.removeFromSuperview()
+        }
+    }
+    
+    @IBAction func toggleCircle(_ sender: UISwitch) {
+
+        if sender.isOn {
+            let newShape = Shape(frame: CGRect(x: 100, y: 190, width: 150, height: 150))
+            newShape.shapeTypeLabel = "Circle"
+            newShape.shapeColor = UIColor.magenta.cgColor
+            newShape.tag = 3
+            self.view.addSubview(newShape)
+        } else {
+            let shapeToBeRemoved = self.view.viewWithTag(3)
+            shapeToBeRemoved?.removeFromSuperview()
+        }
+    }
+    
+    @IBAction func toggleEllipse(_ sender: UISwitch) {
+        if sender.isOn {
+            let newShape = Shape(frame: CGRect(x: 140, y: 230, width: 225, height: 100))
+            newShape.shapeTypeLabel = "Ellipse"
+            newShape.shapeColor = UIColor.blue.cgColor
+            newShape.tag = 4
+            self.view.addSubview(newShape)
+        } else {
+            let shapeToBeRemoved = self.view.viewWithTag(4)
+            shapeToBeRemoved?.removeFromSuperview()
+        }
+    }
+
+}
+
---
+++ lectures/week3/sample-code/ShapesV3/Readme.txt
@@ -0,0 +1,28 @@
+
+This is the ShapesV3 app.
+
+It is based on the Shapes V2 app.
+
+The app adds the ability to work with single and double taps.
+
+A single tap brings the shape to the foreground (the top of the view hierarchy).
+
+A double tap (with one finger) grows the shape by 10%.
+
+A two-finger tap shrinks the shape by 10%.
+
+~~~~~
+
+Previous version features:
+
+The app adds the ability to drag shapes around the view.
+
+~~~~~
+
+Previous version features:
+
+It is based on the single view app template.
+
+The app enables the user to add and remove 'shapes' to the view.
+
+Each 'shape' is a programmatically-created UIView object.
